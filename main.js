a827154fde913d3bfe7683539299837a5952b398
souf <soufboulealf@gmail.com>
2021-12-22 13:53:30 +0100

    updates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: '/home/pi/Documents/parentalcontrol/.env' })
 const { logger } = require('./logger.js')
 const Browser = require('./browser.js')
 const { CronJob } = require('cron')
